7eac4123bafe67dd6788a10dc7e18e08b2160663
Yannick Lescure <yannick.lescure@gmail.com>
2021-04-01 11:40:48 -0400

    improve code + fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
       {
         hid: 'twitter:image',
         name: 'twitter:image',
-        content: 'https://www.laplacepublique.org/la_place_publique_web_1200.jpg'
+        content: 'https://www.laplacepublique.org/la_place_publique_open_graph.jpg'
       },
 
       // Open Graph
@@ -75,12 +75,12 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'https://www.laplacepublique.org/la_place_publique_web_1200.jpg'
+        content: 'https://www.laplacepublique.org/la_place_publique_open_graph.jpg'
       },
       {
         hid: 'og:image:secure_url',
         property: 'og:image:secure_url',
-        content: 'https://www.laplacepublique.org/la_place_publique_web_1200.jpg'
+        content: 'https://www.laplacepublique.org/la_place_publique_open_graph.jpg'
       },
       {
         hid: 'og:image:alt',
