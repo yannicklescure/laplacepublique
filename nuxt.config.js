9d9396df795bfb7cd979e3475a597f1438a57b28
yannick lescure <yannick.lescure@gmail.com>
2021-06-23 21:55:11 -0400

    improve code + fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -205,9 +205,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  },
-
-  env: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
   }
 }
