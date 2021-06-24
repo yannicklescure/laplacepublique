572ac7245c8de039034d7bc722bb3980e047205b
yannick lescure <yannick.lescure@gmail.com>
2021-06-24 13:07:46 -0400

    improve code + fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   target: 'static',
-  router: {
-    base: '/laplacepublique/'
+  generate: {
+    fallback: true
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
